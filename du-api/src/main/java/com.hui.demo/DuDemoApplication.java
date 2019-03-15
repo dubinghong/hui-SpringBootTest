4cd5bbb1715d422bcbbc54b46787ca4199bd24ff
杜炳宏 <dubinghong@easycc.cc>
2019-03-15 11:00:11 +0800

    #1_初始化框架结构

---
+++ du-api/src/main/java/com.hui.demo/DuDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hui.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuDemoApplication.class, args);
+    }
+}
